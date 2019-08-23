be3518732f8e8c780e3366e33595016ef82d041b
Stefan Cordes <mail@quaddy-services.de>
2019-08-23 16:03:54 +0200

    build on temp drive

--- make.bat
+++ make.bat
@@ -1,39 +1,30 @@
 echo on
-set java_home=c:\programs\currentJDK
+
+set t=%temp%\build-dir-deadline-reminder
+
+set actualDir=%CD%
+
+rmdir %t%\target /s /q
+mkdir %t%\target
+
+if x%java_home%x == xx set java_home=c:\programs\currentJDK
 %java_home%\bin\java -version 2>src\main\resources\make-info.txt
 type src\main\resources\make-info.txt
 if %errorlevel% == 1 goto noJDK
 
 echo %date% >src\main\resources\version.txt
 
-del target\DeadlineReminder-SNAPSHOT.zip
-del target\DeadlineReminder-SNAPSHOT.jar
+robocopy src %t%\src *.* /mir 
+copy pom.xml %t% /y
+
+cd /D %t%
+
 call mvn -U clean install source:jar
-rem  webstart:jnlp
 if %errorlevel% == 1 goto ende
-if not exist target\DeadlineReminder-SNAPSHOT.zip goto ende
-rem if not exist target\DeadlineReminder-SNAPSHOT.jar goto ende
 echo on
 
-
-rem copy target\DeadlineReminder-SNAPSHOT.jar deploy\DeadlineReminder.jar /y
-rem cd deploy
-rem "%JAVA_HOME%\bin\jarsigner" -keystore myKeystore -storepass quaddy DeadlineReminder.jar Quaddy-Services.de
-
-if not exist Q:\. goto noq
-rem pause
-pushd .
-rem copy DeadlineReminder*.* Q:\HTML\server.src\quaddy-services\deadline-reminder /y
-copy target\jnlp\*.* Q:\HTML\server.src\quaddy-services\deadline-reminder /y /d
-copy target\jnlp\*.* Q:\HTML\server\quaddy-services\deadline-reminder /y /d
-copy src\version.txt Q:\HTML\server.src\quaddy-services\deadline-reminder /y
-copy src\versionhistory.htm Q:\HTML\server.src\quaddy-services\deadline-reminder\vhistory.htm /y
-cd /d q:\java\adr.de
-rem java -cp .;bin de.adr.html.DynamicHtml
-java -Djava.net.preferIPv4Stack=true -cp .;bin;lib\commons-net-1.4.1.jar;lib\jakarta-oro-2.0.8.jar de.adr.ftp.FtpUpdate
-popd .
-:noq
-rem call mvn clean
+del %actualDir%\target\*.jar
+copy target\*.jar %actualDir%\target\ /y
 
 goto ende
 
@@ -46,3 +37,5 @@ echo see https://technet.microsoft.com/de-de/sysinternals
 goto ende
 
 :ende
+
+cd /D %actualDir%
